4f26806818c4715a98c9cf4e0c7eb5a330cfc40b
Tamicktom <rickpokemon38@gmail.com>
2024-01-05 10:32:43 -0300

    Add heading to Signin screen

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,5 +1,5 @@
 //* Libraries imports
-import { VStack, Image, Text, Center } from "native-base";
+import { VStack, Image, Text, Center, Heading } from "native-base";
 
 import LogoSvg from "@assets/logo.svg"
 import BackgroundImg from "@assets/background.png";
@@ -15,11 +15,17 @@ export function Signin() {
         // width="100%"
         position="absolute"
       />
-      <Center my={24}>
+      <Center py={24}>
         <LogoSvg />
         <Text color="gray.100" fontSize="sm">
           Treine sua mente e o seu corpo
         </Text>
+      </Center>
+
+      <Center>
+        <Heading color="gray.100" fontSize="xl" pb={6} fontFamily="heading">
+          Acesse sua conta
+        </Heading>
       </Center>
     </VStack>
   );
