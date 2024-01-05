711f161d81812f319fbb4b84453b6ec63d923337
Tamicktom <rickpokemon38@gmail.com>
2024-01-05 09:54:25 -0300

    Add dependencies and update Signin screen

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,6 +1,7 @@
 //* Libraries imports
-import { VStack, Image } from "native-base";
+import { VStack, Image, Text, Center } from "native-base";
 
+import LogoSvg from "@assets/logo.svg"
 import BackgroundImg from "@assets/background.png";
 
 export function Signin() {
@@ -14,6 +15,12 @@ export function Signin() {
         // width="100%"
         position="absolute"
       />
+      <Center my={24}>
+        <LogoSvg />
+        <Text color="gray.100" fontSize="sm">
+          Treine sua mente e o seu corpo
+        </Text>
+      </Center>
     </VStack>
   );
 }
