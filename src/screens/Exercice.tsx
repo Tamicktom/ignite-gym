02ed7d1a20829ad9ae3440f754459cca54fcd0d4
Tamicktom <rickpokemon38@gmail.com>
2024-01-09 08:54:08 -0300

    creating the new screens

---
+++ src/screens/Exercice.tsx
@@ -0,0 +1,10 @@
+//* Libraries imports
+import { Center, Text } from 'native-base';
+
+export function Exercice() {
+  return (
+    <Center flex={1}>
+      <Text>Exercice</Text>
+    </Center>
+  );
+}
---
+++ src/screens/Home.tsx
@@ -0,0 +1,10 @@
+//* Libraries imports
+import { Center, Text } from 'native-base';
+
+export function Home() {
+  return (
+    <Center flex={1}>
+      <Text>Home</Text>
+    </Center>
+  );
+}
