462f6f0b30eb272de35d923418481f336cfb4a9f
Tamicktom <henrique.angelo@enterscience.com.br>
2024-01-10 14:34:46 -0300

    Add FlatList for group selection

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,22 +1,66 @@
 //* Libraries imports
 import { useState } from 'react';
-import { VStack, Text, HStack } from 'native-base';
+import { VStack, Text, HStack, FlatList } from 'native-base';
 
 //* Components imports
 import { HomeHeader } from '../components/HomeHeader';
 import { Group } from '../components/Group';
 
+//* Utils imports
+import { idGenerator } from '@utils/idGenerator';
+
+type Group = {
+  id: string;
+  name: string;
+}
+
+const initGroups: Group[] = [
+  {
+    id: idGenerator(),
+    name: 'costas',
+  },
+  {
+    id: idGenerator(),
+    name: 'bicipes',
+  },
+  {
+    id: idGenerator(),
+    name: 'triceps',
+  },
+  {
+    id: idGenerator(),
+    name: 'peito',
+  }
+];
+
+
 export function Home() {
-  const [groupSelected, setGroupSelected] = useState('costas');
+  const [groups, setGroups] = useState<Group[]>(initGroups);
+  const [groupSelected, setGroupSelected] = useState<string>(initGroups[0].id);
 
   return (
     <VStack flex={1}>
       <HomeHeader />
       <HStack>
-        <Group name="costas" isActive={groupSelected === 'costas'} onPress={() => setGroupSelected('costas')} />
-        <Group name="bicipes" isActive={groupSelected === 'bicipes'} onPress={() => setGroupSelected('bicipes')} />
-        <Group name="triceps" isActive={groupSelected === 'triceps'} onPress={() => setGroupSelected('triceps')} />
-        <Group name="peito" isActive={groupSelected === 'peito'} onPress={() => setGroupSelected('peito')} />
+        <FlatList
+          data={groups}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <Group
+              name={item.name}
+              isActive={item.id === groupSelected}
+              onPress={() => setGroupSelected(() => item.id)}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          _contentContainerStyle={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingX: "8",
+            paddingY: "4",
+          }}
+        />
       </HStack>
     </VStack>
   );
---
+++ src/utils/idGenerator.ts
@@ -0,0 +1,16 @@
+export function idGenerator(): string {
+  const dictionary =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const length = 32;
+  let id = "";
+  for (let i = 0; i < length; i++) {
+    id += dictionary.charAt(Math.floor(Math.random() * dictionary.length));
+  }
+  //shufle again
+  let array = id.split("");
+  array.sort(() => Math.random() - 0.5);
+  id = array.join("");
+
+  //break in groups of 8, like a UUID example: 12345678-1234-1234-1234-123456789012
+  return id.replace(/(.{8})(.{4})(.{4})(.{4})(.{12})/, "$1-$2-$3-$4-$5");
+}
