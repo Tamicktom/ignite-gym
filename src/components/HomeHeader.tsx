214001629c05565a02b4a7801d4f60ff4bb4aa9d
Tamicktom <henrique.angelo@enterscience.com.br>
2024-01-10 14:07:47 -0300

    Add logout button to HomeHeader component

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,17 +1,19 @@
 //* Libraries imports
-import { Heading, VStack, HStack, Text } from "native-base";
+import { TouchableOpacity } from "react-native";
+import { Heading, VStack, HStack, Text, Icon } from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
 
 //* Components imports
 import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader() {
   return (
-    <HStack bg="gray.600" pt="12" pb="4" px="8" alignItems="center">
+    <HStack bg="gray.600" pt="12" pb="4" px="8" alignItems="center" justifyContent="space-between">
       <UserPhoto
         size={16}
         source={{ uri: 'https://github.com/tamicktom.png' }}
       />
-      <VStack pl={4}>
+      <VStack pl={4} flex={1}>
         <Text color="gray.100" fontSize="md">
           Olá,
         </Text>
@@ -19,6 +21,15 @@ export function HomeHeader() {
           Rodrigo
         </Heading>
       </VStack>
+
+      <TouchableOpacity>
+        <Icon
+          as={MaterialIcons}
+          name="logout"
+          size={7}
+          color="gray.200"
+        />
+      </TouchableOpacity>
     </HStack>
   )
 }
