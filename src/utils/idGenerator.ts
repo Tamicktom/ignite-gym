c5e4964d5dd9298c192b351781bca8d8fc614016
Tamicktom <henrique.angelo@enterscience.com.br>
2024-01-10 15:50:48 -0300

    Refactor idGenerator function to return a UUID format

--- src/utils/idGenerator.ts
+++ src/utils/idGenerator.ts
@@ -1,4 +1,4 @@
-export function idGenerator(): string {
+export function idGenerator(): `${string}-${string}-${string}-${string}-${string}` {
   const dictionary =
     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
   const length = 32;
@@ -12,5 +12,8 @@ export function idGenerator(): string {
   id = array.join("");
 
   //break in groups of 8, like a UUID example: 12345678-1234-1234-1234-123456789012
-  return id.replace(/(.{8})(.{4})(.{4})(.{4})(.{12})/, "$1-$2-$3-$4-$5");
+  return id.replace(
+    /(.{8})(.{4})(.{4})(.{4})(.{12})/,
+    "$1-$2-$3-$4-$5"
+  ) as `${string}-${string}-${string}-${string}-${string}`;
 }
