05c69e25489e14f7b919d353a706bbdf0bf8264b
Tamicktom <henrique.angelo@enterscience.com.br>
2024-01-10 14:24:27 -0300

    Add group selection functionality to Home screen

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -4,9 +4,10 @@ import { Text, Pressable, IPressableProps } from "native-base";
 
 interface Props extends IPressableProps {
   name: string;
+  isActive: boolean;
 }
 
-export function Group({ name, ...rest }: Props) {
+export function Group({ name, isActive, ...rest }: Props) {
   return (
     <Pressable
       mr={3}
@@ -19,13 +20,19 @@ export function Group({ name, ...rest }: Props) {
       overflow="hidden"
       borderColor="transparent"
       borderWidth={1}
+      isPressed={isActive}
       _pressed={{
         borderColor: "green.500",
         borderWidth: 1
       }}
       {...rest}
     >
-      <Text color="gray.200" textTransform="uppercase" fontSize="xs" fontWeight="bold">
+      <Text
+        color={isActive ? "green.500" : "gray.200"}
+        textTransform="uppercase"
+        fontSize="xs"
+        fontWeight="bold"
+      >
         {name}
       </Text>
     </Pressable>
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,23 @@
 //* Libraries imports
-import { VStack, Text } from 'native-base';
+import { useState } from 'react';
+import { VStack, Text, HStack } from 'native-base';
 
 //* Components imports
 import { HomeHeader } from '../components/HomeHeader';
 import { Group } from '../components/Group';
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState('costas');
+
   return (
     <VStack flex={1}>
       <HomeHeader />
-      <Group name="Family" />
+      <HStack>
+        <Group name="costas" isActive={groupSelected === 'costas'} onPress={() => setGroupSelected('costas')} />
+        <Group name="bicipes" isActive={groupSelected === 'bicipes'} onPress={() => setGroupSelected('bicipes')} />
+        <Group name="triceps" isActive={groupSelected === 'triceps'} onPress={() => setGroupSelected('triceps')} />
+        <Group name="peito" isActive={groupSelected === 'peito'} onPress={() => setGroupSelected('peito')} />
+      </HStack>
     </VStack>
   );
 }
