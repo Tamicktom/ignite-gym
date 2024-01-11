fc63f1723b0ec6b9e69dfdd1f1db728b954a36b0
Tamicktom <henrique.angelo@enterscience.com.br>
2024-01-11 09:03:03 -0300

    Add section list and exercise data to History screen

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,15 +1,41 @@
 //* Libraries imports
-import { VStack } from 'native-base';
+import { useState } from 'react';
+import { Heading, SectionList, VStack } from 'native-base';
 
 //* Component imports
 import { ScreenHeader } from '@components/ScreenHeader';
 import { HistoryCard } from '@components/HistoryCard';
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    { title: 'Costas', data: ['Puxada frontal', 'Remada baixa'] },
+    { title: 'Peito', data: ['Supino reto', 'Supino inclinado'] },
+    { title: 'Ombros', data: ['Desenvolvimento', 'Elevação lateral'] },
+    { title: 'Bíceps', data: ['Rosca direta', 'Rosca alternada'] },
+    { title: 'Tríceps', data: ['Tríceps pulley', 'Tríceps testa'] },
+    { title: 'Pernas', data: ['Agachamento', 'Leg press'] },
+  ])
+
   return (
     <VStack flex={1}>
       <ScreenHeader title='Histórico de exercicios' />
-      <HistoryCard />
+      <SectionList
+        flex={1}
+        sections={exercises}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => <HistoryCard />}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" pt={10} pb={3}>
+            {section.title}
+          </Heading>
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingHorizontal: 16
+        }}
+        ItemSeparatorComponent={() => <VStack h="3" />}
+        paddingBottom={100}
+      />
     </VStack>
   );
 }
