325eeaadc944d1fa4436ded3424290363db6967e
Tamicktom <henrique.angelo@enterscience.com.br>
2024-01-11 08:51:10 -0300

    Add HistoryCard component to History screen

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -3,11 +3,13 @@ import { VStack } from 'native-base';
 
 //* Component imports
 import { ScreenHeader } from '@components/ScreenHeader';
+import { HistoryCard } from '@components/HistoryCard';
 
 export function History() {
   return (
     <VStack flex={1}>
       <ScreenHeader title='Histórico de exercicios' />
+      <HistoryCard />
     </VStack>
   );
 }
