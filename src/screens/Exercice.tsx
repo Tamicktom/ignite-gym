51af60994c9adec4b60c4594243d628d694d3130
Tamicktom <rickpokemon38@gmail.com>
2024-01-11 10:55:36 -0300

    asdas

--- src/screens/Exercice.tsx
+++ src/screens/Exercice.tsx
@@ -1,11 +1,13 @@
 //* Libraries imports
 import { TouchableOpacity } from 'react-native';
-import { Center, Text, VStack, Icon } from 'native-base';
+import { Center, Text, VStack, Icon, HStack, Heading } from 'native-base';
 import { Feather } from "@expo/vector-icons";
 import { useNavigation } from '@react-navigation/native';
 
 //* Utils imports
 import type { AppNavigatorRoutesProps } from "@routes/app.routes";
+
+import BodySvg from "@assets/body.svg";
 
 export function Exercice() {
   const navigation = useNavigation<AppNavigatorRoutesProps>();
@@ -25,6 +27,19 @@ export function Exercice() {
             size={6}
           />
         </TouchableOpacity>
+
+        <HStack justifyContent="space-between" pt={4} pb={8} alignItems="center">
+          <Heading color="gray.100" fontSize="lg" flexShrink={1}>
+            Puxada Frontal
+          </Heading>
+
+          <HStack alignItems="center">
+            <BodySvg />
+            <Text color="gray.200" pl={1} textTransform="capitalize">
+              Costas
+            </Text>
+          </HStack>
+        </HStack>
       </VStack>
     </VStack>
   );
