327668d6b53439f59f2a0aa4d4f1e55e463ca5e1
Tamicktom <henrique.angelo@enterscience.com.br>
2024-01-11 10:16:00 -0300

    Update Profile screen with new components

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,11 +1,13 @@
 //* Libraries imports
 import { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
-import { Center, VStack, ScrollView, Skeleton, Text } from 'native-base';
+import { Center, VStack, ScrollView, Skeleton, Heading } from 'native-base';
 
 //* Components imports
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from "@components/UserPhoto";
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
 
 const PHOTO_SIZE = 32;
 
@@ -26,18 +28,59 @@ export function Profile() {
                 startColor="gray.500"
                 endColor="gray.400"
               />
-              : <UserPhoto
-                size={PHOTO_SIZE}
-                source={{ uri: "https://github.com/tamicktom.png" }}
-              />
+              : <TouchableOpacity>
+                <UserPhoto
+                  size={PHOTO_SIZE}
+                  source={{ uri: "https://github.com/tamicktom.png" }}
+                />
+              </TouchableOpacity>
           }
 
-          <TouchableOpacity>
-            <Text color="green.500" fontWeight="bold" fontSize="md" pt={2} pb={8}>
-              Alterar foto
-            </Text>
-          </TouchableOpacity>
+          <Center w="full" pt="8">
+            <Input
+              placeholder="Digite seu nome"
+              defaultValue="Tamicktom"
+              bg="gray.600"
+            />
+
+            <Input
+              placeholder="Digite seu e-mail"
+              defaultValue="exemplo@exemplo.com"
+              bg="gray.600"
+              isDisabled
+            />
+          </Center>
         </Center>
+
+        <VStack px={10} pt={12} pb={10}>
+          <Heading color="gray.200" fontSize="md" pb="1">
+            Alterar senha
+          </Heading>
+
+          <Input
+            placeholder="Senha antiga"
+            bg="gray.600"
+            secureTextEntry
+          />
+
+          <Input
+            placeholder="Nova senha"
+            bg="gray.600"
+            secureTextEntry
+          />
+
+          <Input
+            placeholder="Confirmar nova senha"
+            bg="gray.600"
+            secureTextEntry
+          />
+
+          <Center w="full" pt="8">
+            <Button
+              label="Atualizar"
+            />
+          </Center>
+        </VStack>
       </ScrollView>
     </VStack>
   );
