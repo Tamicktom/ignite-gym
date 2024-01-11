3be6a393cd2a07f2e2c80f3c6d4aadb1d29ce603
Tamicktom <rickpokemon38@gmail.com>
2024-01-11 10:47:51 -0300

    sabing

--- src/screens/Exercice.tsx
+++ src/screens/Exercice.tsx
@@ -1,10 +1,31 @@
 //* Libraries imports
-import { Center, Text } from 'native-base';
+import { TouchableOpacity } from 'react-native';
+import { Center, Text, VStack, Icon } from 'native-base';
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from '@react-navigation/native';
+
+//* Utils imports
+import type { AppNavigatorRoutesProps } from "@routes/app.routes";
 
 export function Exercice() {
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  const handleGoBack = () => {
+    navigation.goBack();
+  }
+
   return (
-    <Center flex={1}>
-      <Text>Exercice</Text>
-    </Center>
+    <VStack flex={1}>
+      <VStack px={8} bg="gray.600" pt={12}>
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon
+            as={Feather}
+            name="arrow-left"
+            color="green.500"
+            size={6}
+          />
+        </TouchableOpacity>
+      </VStack>
+    </VStack>
   );
 }
